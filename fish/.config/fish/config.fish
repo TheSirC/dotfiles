b1d926b62bbc0d392f16851a36f119c14847b77a
TheSirC <git@r-v-d.fr>
2020-11-30 19:50:31 +0100

    Ajout d'abréviations Fish
    Oui et gros changement de permissions. Pourquoi ? Je ne sais pas.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,7 +7,6 @@ abbr -a em 'emacs -nw'
 abbr -a ffs 'firefox -asearch'
 abbr -a ffsy 'firefox https://www.youtube.com/results\?search_query='
 
-abbr -a fi 'find . -iname'
 abbr -a g 'rg'
 abbr -a e 'exa'
 
@@ -15,6 +14,10 @@ abbr -a e 'exa'
 abbr -a ... 'cd ../..'
 abbr -a .... 'cd ../../..'
 abbr -a ..... 'cd ../../../..'
+
+abbr -a nei 'nix-env -q --installed'
+abbr -a ncu 'nix-channel --update'
+abbr -a nbe 'nix-build -E "with import <nixpkgs> {}; callPackage  {}"'
 
 # Nix-rebuild
 abbr -a nrt 'sudo nixos-rebuild test --fast'
