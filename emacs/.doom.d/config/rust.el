87c8d0bd852dccf7b92aa397087099bf7a6b8d4c
TheSirC <git@r-v-d.fr>
2020-11-30 19:52:02 +0100

    Configuration LSP pour Rust

--- emacs/.doom.d/config/rust.el
+++ emacs/.doom.d/config/rust.el
@@ -1,27 +1,28 @@
 ;; LSP Server settings
 (after! rustic
-  (custom-set-variables
-   '(lsp-rust-analyzer-display-chaining-hints t)
-   '(lsp-rust-analyzer-display-parameter-hints nil)
-   '(lsp-rust-analyzer-macro-expansion-method (quote rustic-analyzer-macro-expand))
-   '(lsp-rust-analyzer-server-command (quote ("/~/.cargo/bin/rust-analyzer")))
-   '(lsp-rust-analyzer-server-display-inlay-hints nil)
-   '(lsp-rust-full-docs t)
-   '(lsp-rust-server (quote rust-analyzer))
-   '(rustic-lsp-server (quote rust-analyzer))
-   '(lsp-ui-doc-alignment (quote window))
-   '(lsp-ui-doc-position (quote top))
-   '(lsp-ui-sideline-enable nil)
-   )
-  (push 'rustic-clippy flycheck-checkers)
-  (add-hook 'eglot--managed-mode-hook (lambda () (flymake-mode -1)))
-  (remove-hook 'rustic-mode-hook 'flycheck-mode) ; Turn off flycheck.
+  (setq
+   lsp-rust-full-docs t
+   lsp-rust-analyzer-macro-expansion-method (quote rustic-analyzer-macro-expand)
+   lsp-rust-analyzer-server-command "rust-analyzer"
+   lsp-rust-analyzer-cargo-watch-command "clippy"
+   ;; LSP Hints
+   lsp-rust-analyzer-display-chaining-hints t
+   lsp-rust-analyzer-display-parameter-hints t
+   lsp-rust-analyzer-server-display-inlay-hints t
 
-  ;; Client side settings
-  (setq rustic-lsp-client 'eglot)
-  (setq rustic-format-trigger 'on-save)
-  (setq rustic-format-on-save t)
-  )
+   lsp-ui-doc-alignment (quote window)
+   lsp-ui-doc-position (quote top)
+   ;; Rustic-specific settings
+   rustic-lsp-server (quote rust-analyzer)
+   rustic-lsp-client (quote lsp-mode) ; Client side settings
+   rustic-lsp-format t
+   rustic-format-trigger (quote on-compile))
+
+
+  (after! smartparens
+    (sp-with-modes 'rustic
+      (sp-local-pair "'" nil))) (setq rustic-format-on-save t))
+
 
 ;; for Christ's sake evil-surround those "<" aren't tags
 (add-hook 'rust-mode-hook (lambda ()
